28e040abb5e6e58821d99414a77a9fda65d69a33
Lucas Daniel <ldaniel@montanasteele.com>
2020-01-24 09:27:41 -0500

    add Insert sort part 2

---
+++ algorithms/correctness-and-the-loop-invariant/correctness-and-the-loop-invariant.php
@@ -0,0 +1,22 @@
+<?php
+
+function insertionSort(&$arr){
+   for($i=0;$i<count($arr);$i++){
+      $val = $arr[$i];
+      $j = $i-1;
+      while($j>0 && $arr[$j] > $val){
+         $arr[$j+1] = $arr[$j];
+         $j--;
+      }
+      $arr[$j+1] = $val;
+   }
+}
+
+$handle = fopen ("php://stdin","r");
+$t = fgets($handle);
+$arr = explode(' ', fgets($handle));
+
+insertionSort($arr);
+foreach($arr as $value) {
+  print $value." ";
+}
