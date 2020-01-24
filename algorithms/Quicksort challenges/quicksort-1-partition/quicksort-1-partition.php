97007ba9f9ca9eb6cbd9498844068a3564cfa1c0
Lucas Daniel <ldaniel@montanasteele.com>
2020-01-24 15:50:56 -0500

    add quicksort 1

--- algorithms/Quicksort challenges/quicksort-1-partition/quicksort-1-partition.php
+++ algorithms/Quicksort challenges/quicksort-1-partition/quicksort-1-partition.php
@@ -3,6 +3,19 @@
 // Complete the quickSort function below.
 function quickSort($arr)
 {
+    $c            = count($arr);
+    $pivot        = $arr[0];
+    $temp         = 0;
+    for ($i = 1; $i < $c; $i++) {
+        if ($pivot > $arr[$i]) {
+            for ($j = $i; $j > 0; $j--) {
+                $temp = $arr[$j];
+                $arr[$j] = $arr[$j - 1];
+                $arr[$j - 1] = $temp;
+            }
+        }
+    }
+    return $arr;
 }
 
 $fptr = fopen(getenv("OUTPUT_PATH"), "w");
