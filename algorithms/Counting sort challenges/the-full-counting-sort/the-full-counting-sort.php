edbcfea7522b8cd6c50a04a4891331e5355b4d29
Lucas Daniel <ldaniel@montanasteele.com>
2020-01-30 16:13:15 -0500

    add full counting sort

--- algorithms/Counting sort challenges/the-full-counting-sort/the-full-counting-sort.php
+++ algorithms/Counting sort challenges/the-full-counting-sort/the-full-counting-sort.php
@@ -1,18 +1,55 @@
 <?php
-
-// Complete the countSort function below.
-function countSort($arr)
+// Complete the countingSort function below.
+function countSort($fp, $n)
 {
+    $resultSort     = array();
+    $out            = array();
+    $arr            = array();
+    $arrTemp        = array();
+    $arrSort        = array();
+    $count          = 0;
+
+    while ($arr = trim(fgets($fp))) {
+        $count++;
+        list($a, $t) = explode(' ', $arr);
+        if ($count <= $n / 2) {
+            $t = '-';
+        }
+        $arrTemp[(int) $a][] = $t;
+        $arrSort[] = $a;
+    }
+
+    //sort array
+    for ($i = 0; $i < 100; $i++) {
+        if (!isset($resultSort[$i]))
+            $resultSort[$i] = 0;
+    }
+
+    $j      = count($arrSort);
+    for ($i = 0; $i < $j; $i++) {
+        if (!isset($resultSort[$arrSort[$i]]))
+            $resultSort[$arrSort[$i]] = 1;
+        else
+            $resultSort[$arrSort[$i]]++;
+    }
+
+    $c      = count($resultSort);
+    $j      = 0;
+
+    for ($i = 0; $i < $c; $i++) {
+        for ($j = 0; $j < $resultSort[$i]; $j++) {
+            if (isset($arrTemp[$i][$j]))
+                $out[] = $arrTemp[$i][$j];
+        }
+    }
+
+    return $out;
 }
 
-$n = intval(trim(fgets(STDIN)));
+$fp = fopen("php://stdin", "r");
 
-$arr == array();
+$n = intval(trim(fgets($fp)));
 
-for ($i = 0; $i < $n; $i++) {
-    $arr_temp = rtrim(fgets(STDIN));
+$result = countSort($fp, $n);
 
-    $arr[] = preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY);
-}
-
-countSort($arr);
+echo implode(' ', $result);
